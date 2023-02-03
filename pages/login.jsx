0096667609b71e2b55f50fd25405b8657352b0ae
divyansh343 <divyanshpal402@gmail.com>
2023-02-03 16:04:05 +0530

    debug fir se

--- pages/login.jsx
+++
@@ -1,7 +0,0 @@
-import { getSession, useSession } from "next-auth/react"
-import LoginComponents from "../components/LoginComponents"
-
-const login = () => {
-    return <LoginComponents/>
-}
-export default login
