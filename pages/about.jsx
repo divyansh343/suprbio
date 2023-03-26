4ce438615fd9a7d6209d875afa0eebea19a16e20
divyansh343 <divyanshpal402@gmail.com>
2023-03-26 12:16:59 +0530

    profile page updated 1.1

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MEditor from '../components/MEditor'
+
+const about = () => {
+  return (
+    <div>
+      <MEditor />
+    </div>
+  )
+}
+
+export default about
